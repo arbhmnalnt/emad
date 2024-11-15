4431d2da0637b6515e83559a04dc3699fa7f3286
Ahmed Sayed <ahmedsayed124680@gmail.com>
2024-11-15 22:25:35 +0200

    try trans

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.landing, name='landing'),
+    path('set_language/', views.set_language, name='set_language'),
+    path('play/', views.landing, name='play_game'),  # This defines the 'play_game' URL
+    path('instructions/', views.landing, name='instructions'),  # This defines the 'instructions' URL
+    path('high_scores/', views.landing, name='high_scores'),  # This defines the 'high_scores' URL
+
+
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-def home(request):
-    return render(request, 'frontend/index.html')
+from django.shortcuts import render, redirect
+from django.utils.translation import activate
+
+def landing(request):
+    print(f'{request.session["django_language"]}')
+    LANGUAGE_CODE = request.session["django_language"]
+    ctx = {'LANGUAGE_CODE':LANGUAGE_CODE}
+    return render(request, 'frontend/landing.html', ctx)
+
+def set_language(request):
+    if request.method == 'POST':
+        language = request.POST.get('language')
+        if language in ['en', 'ar']:
+            activate(language)
+            request.session['django_language'] = language
+    return redirect('landing')
